2c88fb703a1408fd5a5752fe4ae77b208da1b88a
WavyVeem0 <94560527+WavyVeem0@users.noreply.github.com>
2026-06-22 23:12:00 +0300

    Create client1_base.nix

---
+++ client1_base.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "vm1-client";
+  networking.useDHCP = false;
+  
+  networking.interfaces.ens33.ipv4.addresses = [{
+    address = "192.168.13.10";
+    prefixLength = 24;
+  }];
+  
+  # Шлюз по умолчанию указывает на ВМ 2
+  networking.defaultGateway = "192.168.13.1";
+}
